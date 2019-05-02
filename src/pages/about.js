218f12aba63ba6488859627b4eb21b9ca5654808
nedmorgan <nmorgan1987@gmail.com>
2019-05-02 09:43:59 -0400

    created a new about page

---
+++ src/pages/about.js
@@ -0,0 +1,8 @@
+import React from "react"
+
+export default () => (
+  <div style={{ color: `teal` }}>
+    <h1>About Gatsby</h1>
+    <p>Super cool. How React!</p>
+  </div>
+)
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,21 @@
 import React from "react"
 
 export default () => (
-  <div style={{ color: `tomato`, width: `100%` }}>
-    <h1 style={{ fontSize: `48px`, textAlign: `center` }}>Hello Walter!</h1>
-    <p style={{ fontSize: `24px`, textAlign: `center` }}>What a world!</p>
+  <div
+    style={{
+      color: `tomato`,
+      width: `100%`,
+      display: `flex`,
+      flexDirection: `column`,
+      alignItems: `center`,
+    }}
+  >
+    <h1 style={{ fontSize: `48px` }}>Hello Walter!</h1>
+    <p style={{ fontSize: `24px` }}>What a world!</p>
+    <img
+      style={{ borderRadius: `5px` }}
+      src="https://source.unsplash.com/random/400x200"
+      alt=""
+    />
   </div>
 )
