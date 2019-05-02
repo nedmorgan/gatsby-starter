5c47ecfff40033a0914b7835c8022b9942c582de
nedmorgan <nmorgan1987@gmail.com>
2019-05-02 09:21:37 -0400

    changed some text and added styling to local gatsby site

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,8 @@
 import React from "react"
 
-export default () => <div>Hello world!</div>
+export default () => (
+  <div style={{ color: `tomato`, width: `100%` }}>
+    <h1 style={{ fontSize: `48px`, textAlign: `center` }}>Hello Walter!</h1>
+    <p style={{ fontSize: `24px`, textAlign: `center` }}>What a world!</p>
+  </div>
+)
